ddf3be47a1b2dd4f73b58d8bb1239fa9bb2cf421
Shehryar169 <khanshehryar169@gmail.com>
2019-03-13 22:20:24 +0500

    Adding some relations

--- app/Customer.php
+++ app/Customer.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
     //
+
+
+    public function catagory()
+    {
+        return $this->hasOne('App/customer_catagory');
+    }
 }
